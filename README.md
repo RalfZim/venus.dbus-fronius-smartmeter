acd60df3b78739a1f52101403abad76a7be0b095
Ralf Zimmermann InfinityPro <mail@ralfzimmermann.de>
2020-04-23 16:14:07 +0200

    First working version and readme

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# venus.dbus-fronius-smartmeter
-A service for Venus OS, reading smart meter data from a Fronius system and making the values available on dbus.
+# dbus-fronius-smartmeter Service
+
+### Purpose
+
+This service is meant to be run on a raspberry Pi with Venus OS from Victron.
+
+The Python script cyclically reads data from the Fronius SmartMeter via the Fronius REST API and publishes information on the dbus, using the service name com.victronenergy.grid. This makes the Venus OS work as if you had a physical Victron Grid Meter installed.
+
+### Configuration
+
+In the Python file, you should put the IP of your Fronius device that hosts the REST API. In my setup, it is the IP of the Fronius Symo, which gets the data from the Fronius Smart Metervia the RS485 connection between them.
+
+### Installation
+
+1. Copy the files to the /data folder on your venus:
+
+   - /data/dbus-fronius-smartmeter/dbus-fronius-smartmeter.py
+   - /data/dbus-fronius-smartmeter/service/run
+
+2. Get two files from the [velib_python](https://github.com/victronenergy/velib_python) and install them on your venus:
+
+   - /data/dbus-fronius-smartmeter/vedbus.py
+   - /data/dbus-fronius-smartmeter/ve_utils.py
+
+3. Add a symlink to the file /data/rc.local:
+    `ln -s /data/dbus-fronius-smartmeter/service /service/dbus-fronius-smartmeter`
+
+  Or if that file does not exist yet, store the file rc.local from this service on your Raspberry Pi as /data/rc.local .
+
+  You can then create the symlink by just running rc.local:
+     `rc.local`
+
+  The daemon-tools should automatically start this service within seconds.
+
+### Debugging
+
+You can check the status of the service with svstat:
+`svstat /service/dbus-fronius-smartmeter`
+It will show something like this:
+`/service/dbus-fronius-smartmeter: up (pid 10078) 325 seconds`
+
+If the number of seconds is always 0 or 1 or any other small number, it means that the service crashes and gets restarted all the time.
+
+When you think that the script crashes, start it directly from the command line:
+`python /data/dbus-fronius-smartmeter/dbus-fronius-smartmeter.py`
+and see if it throws any error messages.
+
+If the script stops with the message
+`dbus.exceptions.NameExistsException: Bus name already exists: com.victronenergy.grid"`
+it means that the service is still running or another service is using that bus name.
+
+
---
+++ dbus-fronius-smartmeter.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+"""
+Created by Ralf Zimmermann (mail@ralfzimmermann.de) in 2020.
+This code and its documentation can be found on: https://github.com/RalfZim/venus.dbus-fronius-smartmeter
+Used https://github.com/victronenergy/velib_python/blob/master/dbusdummyservice.py as basis for this service.
+Reading information from the Fronius Smart Meter via http REST API and puts the info on dbus.
+"""
+import gobject
+import platform
+import logging
+import sys
+import os
+import requests # for http GET
+import thread   # for daemon = True
+
+# our own packages
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '../ext/velib_python'))
+from vedbus import VeDbusService
+
+class DbusDummyService:
+  def __init__(self, servicename, deviceinstance, paths, productname='Fronius Smart Meter', connection='Fronius Smart Meter service'):
+    self._dbusservice = VeDbusService(servicename)
+    self._paths = paths
+
+    logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
+
+    # Create the management objects, as specified in the ccgx dbus-api document
+    self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+    self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
+    self._dbusservice.add_path('/Mgmt/Connection', connection)
+
+    # Create the mandatory objects
+    self._dbusservice.add_path('/DeviceInstance', deviceinstance)
+    self._dbusservice.add_path('/ProductId', 16) # value used in ac_sensor_bridge.cpp of dbus-cgwacs
+    self._dbusservice.add_path('/ProductName', productname)
+    self._dbusservice.add_path('/FirmwareVersion', 0.1)
+    self._dbusservice.add_path('/HardwareVersion', 0)
+    self._dbusservice.add_path('/Connected', 1)
+
+    for path, settings in self._paths.iteritems():
+      self._dbusservice.add_path(
+        path, settings['initial'], writeable=True, onchangecallback=self._handlechangedvalue)
+
+    gobject.timeout_add(200, self._update) # pause 200ms before the next request
+
+  def _update(self):
+    URL = "http://10.194.65.143/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0&DataCollection=MeterRealtimeData"
+    meter_r = requests.get(url = URL)
+    meter_data = meter_r.json() 
+    MeterConsumption = meter_data['Body']['Data']['PowerReal_P_Sum']
+    self._dbusservice['/Ac/Power'] = MeterConsumption # positive: consumption, negative: feed into grid
+    self._dbusservice['/Ac/L1/Voltage'] = meter_data['Body']['Data']['Voltage_AC_Phase_1']
+    self._dbusservice['/Ac/L2/Voltage'] = meter_data['Body']['Data']['Voltage_AC_Phase_2']
+    self._dbusservice['/Ac/L3/Voltage'] = meter_data['Body']['Data']['Voltage_AC_Phase_3']
+    self._dbusservice['/Ac/L1/Current'] = meter_data['Body']['Data']['Current_AC_Phase_1']
+    self._dbusservice['/Ac/L2/Current'] = meter_data['Body']['Data']['Current_AC_Phase_2']
+    self._dbusservice['/Ac/L3/Current'] = meter_data['Body']['Data']['Current_AC_Phase_3']
+    self._dbusservice['/Ac/L1/Power'] = meter_data['Body']['Data']['PowerReal_P_Phase_1']
+    self._dbusservice['/Ac/L2/Power'] = meter_data['Body']['Data']['PowerReal_P_Phase_2']
+    self._dbusservice['/Ac/L3/Power'] = meter_data['Body']['Data']['PowerReal_P_Phase_3']
+    self._dbusservice['/Ac/Energy/Forward'] = meter_data['Body']['Data']['EnergyReal_WAC_Sum_Consumed']
+    self._dbusservice['/Ac/Energy/Reverse'] = meter_data['Body']['Data']['EnergyReal_WAC_Sum_Produced']
+    logging.info("House Consumption: %s" % (MeterConsumption))
+    return True
+
+  def _handlechangedvalue(self, path, value):
+    logging.debug("someone else updated %s to %s" % (path, value))
+    return True # accept the change
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+  thread.daemon = True # allow the program to quit
+
+  from dbus.mainloop.glib import DBusGMainLoop
+  # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+  DBusGMainLoop(set_as_default=True)
+
+  pvac_output = DbusDummyService(
+    servicename='com.victronenergy.grid',
+    deviceinstance=0,
+    paths={
+      '/Ac/Power': {'initial': 0},
+      '/Ac/L1/Voltage': {'initial': 0},
+      '/Ac/L2/Voltage': {'initial': 0},
+      '/Ac/L3/Voltage': {'initial': 0},
+      '/Ac/L1/Current': {'initial': 0},
+      '/Ac/L2/Current': {'initial': 0},
+      '/Ac/L3/Current': {'initial': 0},
+      '/Ac/L1/Power': {'initial': 0},
+      '/Ac/L2/Power': {'initial': 0},
+      '/Ac/L3/Power': {'initial': 0},
+      '/Ac/Energy/Forward': {'initial': 0}, # energy bought from the grid
+      '/Ac/Energy/Reverse': {'initial': 0}, # energy sold to the grid
+    })
+
+  logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
+  mainloop = gobject.MainLoop()
+  mainloop.run()
+
+if __name__ == "__main__":
+  main()
